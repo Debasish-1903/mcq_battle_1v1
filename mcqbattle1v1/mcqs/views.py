ed47cf0177ceda1f1aa5202ddacdcd3b3b39e873
Debasish-1903 <das31debasish@gmail.com>
2024-08-15 12:36:17 +0530

    minor fixes

--- mcqbattle1v1/mcqs/views.py
+++ mcqbattle1v1/mcqs/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.permissions import IsAuthenticated
-from .serializer import McqSerializer
+from .serializer import McqSerializer,GameSerializer
 from .models import MCQs
 from rest_framework.response import Response
 from rest_framework import status
+from .models import Game
+
 
 # Create your views here.
 
@@ -78,5 +80,55 @@ class McqRetrieveUpdateDestroyView(APIView):
              mcq.delete()
              return Response(status=status.HTTP_204_NO_CONTENT)
        
+
+
+class GameListCreateView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+        games = Game.objects.all()
+        serializer = GameSerializer(games, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    def post(self, request):
+        serializer = GameSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save(owner=request.user)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
        
-                 
+
+
+class GameRetrieveUpdateDestroyView(APIView):
+     permission_classes = [IsAuthenticated]
+
+     def get_object(self, pk):
+          try:
+               return Game.objects.get(pk=pk)
+          except Game.DoesNotExist:
+               return None
+
+     def get(self, request, pk):
+          game = self.get_object(pk)
+          if game is None:
+               return Response(status=status.HTTP_404_NOT_FOUND)
+          serializer = GameSerializer(game)
+          return Response(serializer.data, status=status.HTTP_200_OK)
+
+     def put(self, request, pk):
+          game = self.get_object(pk)
+          if game is None:
+               return Response(status=status.HTTP_404_NOT_FOUND)
+          serializer = GameSerializer(game, data=request.data)
+          if serializer.is_valid():
+               serializer.save()
+               return Response(serializer.data, status=status.HTTP_200_OK)
+          return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+     def delete(self, request, pk):
+          game = self.get_object(pk)
+          if game is None:
+               return Response(status=status.HTTP_404_NOT_FOUND)
+          game.delete()
+          return Response(status=status.HTTP_204_NO_CONTENT)
