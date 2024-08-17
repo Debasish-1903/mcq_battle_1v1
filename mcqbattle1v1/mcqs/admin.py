adfaf8d1aaf42ada3591e95bdda6eb69f2653901
Debasish-1903 <das31debasish@gmail.com>
2024-08-17 20:44:37 +0530

    modified admin

--- mcqbattle1v1/mcqs/admin.py
+++ mcqbattle1v1/mcqs/admin.py
@@ -1,13 +1,7 @@
 from django.contrib import admin
-from .models import Game, MCQs
-
-@admin.register(Game)
-class GameAdmin(admin.ModelAdmin):
-    list_display = ('id', 'title', 'owner', 'status', 'created_at', 'started_at', 'completed_at')
-    list_filter = ('status', 'created_at', 'started_at', 'completed_at')
-    search_fields = ('title', 'owner__username')
+from .models import  MCQs
 
 @admin.register(MCQs)
 class MCQsAdmin(admin.ModelAdmin):
-    list_display = ('id', 'body', 'explanation')
+    list_display = ('id', 'body', 'explanation') #one tuple
     search_fields = ('body', 'explanation')
