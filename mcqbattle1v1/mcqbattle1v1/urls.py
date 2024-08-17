3cbdf4335f9fb8c92375ecb197c618b20fa04a43
Debasish-1903 <das31debasish@gmail.com>
2024-08-17 20:43:54 +0530

    modified urls

--- mcqbattle1v1/mcqbattle1v1/urls.py
+++ mcqbattle1v1/mcqbattle1v1/urls.py
@@ -14,10 +14,12 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+'''
 from django.contrib import admin
 from django.urls import path
 from auth_app.views import RegisterView,LoginView,ProtectedView,GetView
-from mcqs.views import McqListCreateView,McqRetrieveUpdateDestroyView ,GameListCreateView,GameRetrieveUpdateDestroyView
+from mcqs.views import McqListCreateView,McqRetrieveUpdateDestroyView 
+from game.views import GameListCreateView,GameRetrieveUpdateDestroyView
 
 urlpatterns = [
     
@@ -33,4 +35,20 @@ urlpatterns = [
     path('games/', GameListCreateView.as_view(), name='game-list-create'),
     path('games/<uuid:pk>/', GameRetrieveUpdateDestroyView.as_view(), name='game-detail'),
 
+]
+'''
+from django.contrib import admin
+from django.urls import path, include
+from auth_app.views import RegisterView, LoginView, ProtectedView, GetView
+from mcqs.views import McqListCreateView, McqRetrieveUpdateDestroyView
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('register/', RegisterView.as_view(), name='register'),
+    path('login/', LoginView.as_view(), name='login'),
+    path('protected/', ProtectedView.as_view(), name='protected'),
+    path('get/', GetView.as_view(), name='get'),
+    path('mcqs/', McqListCreateView.as_view(), name='mcq-list-create'),
+    path('mcqs/<uuid:pk>/', McqRetrieveUpdateDestroyView.as_view(), name='mcq-detail'),
+    path('games/', include('game.urls')),  
 ]
